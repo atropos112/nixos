3d608c9a592961e1b3a56956ab93731e5dd9310a
atropos <sv7n@pm.me>
2024-01-20 20:01:39 +0000

    Working on NixVim continues, its not finished or documented even.

--- lib/pkgs/nixvim.nix
+++ lib/pkgs/nixvim.nix
@@ -1,4 +1,8 @@
-{inputs, ...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
   imports = [
     inputs.nixvim.nixosModules.nixvim
   ];
@@ -9,12 +13,100 @@
       relativenumber = true;
       shiftwidth = 2;
     };
+    globals.mapleader = ","; # Sets the leader key to comma
+    extraPlugins = with pkgs.vimPlugins; [
+      vim-go
+    ];
+
+    keymaps = [
+      {
+        mode = "n";
+        key = "<Leader>c";
+        action = ":DapContinue<CR>";
+      }
+      {
+        mode = "n";
+        key = "<Leader>0";
+        action = ":DapStepOver<CR>";
+      }
+      {
+        mode = "n";
+        key = "<Leader>-";
+        action = ":DapStepInto<CR>";
+      }
+      {
+        mode = "n";
+        key = ''<Leader>\'';
+        action = ":DapStepOut<CR>";
+      }
+      {
+        mode = "n";
+        key = "<Leader>b";
+        action = ":DapToggleBreakpoint<CR>";
+      }
+    ];
 
     colorschemes.onedark.enable = true;
+
+    extraConfigLua = ''
+      require("dapui").setup()
+      local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+    ''; # Enable the dapui sidebar
+
     plugins = {
       lightline.enable = true;
       luasnip.enable = true;
       treesitter.enable = true;
+
+      telescope = {
+        enable = true;
+        extensions = {
+          fzf-native.enable = true;
+        };
+        keymaps = {
+          "<C-g>" = {
+            action = "git_files";
+            desc = "Telescope Git Files";
+          };
+          "<leader>fg" = "live_grep";
+        };
+      };
+
+      copilot-lua = {
+        enable = true;
+        suggestion = {
+          autoTrigger = true;
+          keymap.accept = "<C-CR>";
+        };
+      };
+
+      dap = {
+        enable = true;
+        extensions = {
+          dap-go.enable = true;
+          dap-ui.enable = true;
+        };
+      };
+      toggleterm = {
+        enable = true;
+        direction = "horizontal";
+        size = 20;
+        openMapping = "<C-p>";
+        shadeTerminals = true;
+        persistSize = true;
+      };
 
       lsp = {
         enable = true;
@@ -29,6 +121,7 @@
       };
 
       lsp-format.enable = true;
+      cmp_luasnip.enable = true;
 
       nvim-cmp = {
         enable = true;
@@ -46,12 +139,6 @@
               function(fallback)
                 if cmp.visible() then
                   cmp.select_next_item()
-                elseif luasnip.expandable() then
-                  luasnip.expand()
-                elseif luasnip.expand_or_jumpable() then
-                  luasnip.expand_or_jump()
-                elseif check_backspace() then
-                  fallback()
                 else
                   fallback()
                 end
