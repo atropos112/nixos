22227b15877c990d7fc537c22f8e91be65841ebe
atropos <sv7n@pm.me>
2024-01-27 14:45:26 +0000

    Added nfs server

--- devices/rzr/default.nix
+++ devices/rzr/default.nix
@@ -14,18 +14,23 @@ _: {
   services.nfs.server = {
     enable = true;
     exports = ''
-      /mnt/nfs  9.0.0.0/24(rw,no_subtree_check)
-      /mnt/nfs  127.0.0.1/32(rw,no_subtree_check)
-      /mnt/nfs  10.0.0.0/16(rw,no_subtree_check)
+      /mnt/nfs/longhorn  9.0.0.2(rw,no_subtree_check)
+      /mnt/nfs/longhorn  9.0.0.3(rw,no_subtree_check)
+      /mnt/nfs/longhorn  9.0.0.4(rw,no_subtree_check)
+      /mnt/nfs/longhorn  9.0.0.5(rw,no_subtree_check)
+      /mnt/nfs/longhorn  9.0.0.6(rw,no_subtree_check)
+      /mnt/nfs/longhorn  9.0.0.7(rw,no_subtree_check)
+      /mnt/nfs/longhorn  9.0.0.8(rw,no_subtree_check)
+      /mnt/nfs/longhorn  9.0.0.130(rw,no_subtree_check)
 
-      /mnt/nfs  rzr(rw,no_subtree_check)
-      /mnt/nfs  a21(rw,no_subtree_check)
-      /mnt/nfs  smol(rw,no_subtree_check)
-      /mnt/nfs  opi1(rw,no_subtree_check)
-      /mnt/nfs  opi2(rw,no_subtree_check)
-      /mnt/nfs  opi3(rw,no_subtree_check)
-      /mnt/nfs  opi4(rw,no_subtree_check)
-      /mnt/nfs  giant(rw,no_subtree_check)
+      /mnt/nfs/longhorn  rzr(rw,no_subtree_check)
+      /mnt/nfs/longhorn  a21(rw,no_subtree_check)
+      /mnt/nfs/longhorn  smol(rw,no_subtree_check)
+      /mnt/nfs/longhorn  opi1(rw,no_subtree_check)
+      /mnt/nfs/longhorn  opi2(rw,no_subtree_check)
+      /mnt/nfs/longhorn  opi3(rw,no_subtree_check)
+      /mnt/nfs/longhorn  opi4(rw,no_subtree_check)
+      /mnt/nfs/longhorn  giant(rw,no_subtree_check)
     '';
   };
 }
