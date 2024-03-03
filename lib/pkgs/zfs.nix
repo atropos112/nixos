7c18fdf42aafc38924dcc1c966204315a658e278
atropos <sv7n@pm.me>
2024-03-03 18:51:23 +0000

    Small comment.

--- lib/pkgs/zfs.nix
+++ lib/pkgs/zfs.nix
@@ -25,6 +25,7 @@
   };
 
   # ZFS already has its own scheduler. Without this my(@Artturin) computer froze for a second when i nix build something.
+  # TODO: https://github.com/nix-community/srvos/pull/372 this states its part of nixpkgs already, verify this and if so delete it from here
   services.udev.extraRules = ''
     ACTION=="add|change", KERNEL=="sd[a-z]*[0-9]*|mmcblk[0-9]*p[0-9]*|nvme[0-9]*n[0-9]*p[0-9]*", ENV{ID_FS_TYPE}=="zfs_member", ATTR{../queue/scheduler}="none"
   '';
