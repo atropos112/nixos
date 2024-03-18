b85b7952b6c5792da51f7c3de8896d34ec5cf4a6
atropos <sv7n@pm.me>
2024-03-18 21:58:52 +0000

    WIP on 6.8 kernel for opi5's

--- lib/common/opi5/default.nix
+++ lib/common/opi5/default.nix
@@ -10,7 +10,7 @@
     #./hardware.nix # Non standard, typically this is done from devices/<devicename>/hardware.nix but its the same across and always should be so importing it here instead.
   ];
 
-  kernelPackages = lib.mkForce pkgs.linuxPackagesFor (pkgs.callPackage ./kernel/socle.nix {});
+  boot.kernelPackages = lib.mkForce (pkgs.linuxPackagesFor (pkgs.callPackage ./kernel/socle.nix {}));
 
   # boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
 }
