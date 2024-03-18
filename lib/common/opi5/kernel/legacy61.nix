0f8ec2a5e20edc5a15722f8b7b0e6dd9f9c883bb
atropos <sv7n@pm.me>
2024-03-18 14:13:43 +0000

    CONFIG_BPF_JIT=y and CONFIG_NET_SCH_INGRESS=m

--- lib/common/opi5/kernel/legacy61.nix
+++ lib/common/opi5/kernel/legacy61.nix
@@ -6,16 +6,15 @@
 }:
 (linuxManualConfig {
   src = fetchFromGitHub {
-    owner = "armbian";
-    repo = "linux-rockchip";
-    rev = "f3fb30ac9de06b41fb621d17bc53603f1f48ac90";
+    owner = "orangepi-xunlong";
+    repo = "linux-orangepi";
+    rev = "752c0d0a12fdce201da45852287b48382caa8c0f";
     hash = "sha256-tVu/3SF/+s+Z6ytKvuY+ZwqsXUlm40yOZ/O5kfNfUYc=";
   };
-  version = "6.1.43";
+  version = "6.1.43-xunlong-rk35xx";
   modDirVersion = "6.1.43";
   extraMeta.branch = "6.1";
-  configfile = ./armbian61.config;
-  # configfile = ./linux-6.1.43-xunlong-rk35xx.config;
+  configfile = ./linux-6.1.43-xunlong-rk35xx.config;
   allowImportFromDerivation = true;
 })
 .overrideAttrs (old: {
