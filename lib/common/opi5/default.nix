ac0d43f538ad0e11a363673599986efc207f1fdd
atropos <sv7n@pm.me>
2024-03-19 08:47:20 +0000

    WIP on 6.8 kernel for opi5's

--- lib/common/opi5/default.nix
+++ lib/common/opi5/default.nix
@@ -10,11 +10,11 @@
     #./hardware.nix # Non standard, typically this is done from devices/<devicename>/hardware.nix but its the same across and always should be so importing it here instead.
   ];
 
-  boot.kernelPackages = lib.mkForce (pkgs.linuxPackagesFor (pkgs.callPackage ./kernel/socle.nix {}));
+  # boot.kernelPackages = lib.mkForce (pkgs.linuxPackagesFor (pkgs.callPackage ./kernel/socle.nix {}));
 
   environment.systemPackages = with pkgs; [
     cilium-cli
   ];
 
-  # boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
 }
