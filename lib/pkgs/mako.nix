54f407989bb1b5f9986cefd737b08964b734194a
atropos <sv7n@pm.me>
2024-03-23 17:17:03 +0000

    Fixed mako a bit, i do like time out.

--- lib/pkgs/mako.nix
+++ lib/pkgs/mako.nix
@@ -1,17 +1,31 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: let
+  inherit (config.lib.stylix) colors;
+  makoOpacity = lib.toHexString (((builtins.ceil (config.stylix.opacity.popups * 100)) * 255) / 100);
+in {
   home-manager.users.atropos.services.mako = {
     enable = true;
     package = pkgs.mako;
-    extraConfig = ''
-      font=Inconsolata 14
-      background-color=#151718
-      text-color=#9FCA56
-      border-color=#151718
+    # Got to have it manually as opposed to Stylix
+    # Because Stylix config comes first and has no timeout...
+    extraConfig = lib.mkForce ''
       default-timeout=3000
       ignore-timeout=1
 
+      [urgency=low]
+      background-color=#${colors.base00}${makoOpacity}
+      border-color=#${colors.base0D}
+      text-color=#${colors.base0A}
+      default-timeout=3000
+
       [urgency=high]
-      text-color=#CD3F45
+      background-color=#${colors.base00}${makoOpacity}
+      border-color=#${colors.base0D}
+      text-color=#${colors.base08}
       default-timeout=5000
     '';
   };
