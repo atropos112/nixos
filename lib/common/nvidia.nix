1c7dbc3da846bbdc12213efd260bdef252b6c880
atropos <sv7n@pm.me>
2024-06-28 20:07:18 +0100

    fixed docker nvidia setup

--- lib/common/nvidia.nix
+++ lib/common/nvidia.nix
@@ -4,20 +4,29 @@
   pkgs-stable,
   ...
 }: {
-  virtualisation.docker = {
-    enableNvidia = true;
-    # extraOptions = "--default-runtime=nvidia";
+  virtualisation = {
+    docker = {
+      enableNvidia = true;
+    };
+    containers = {
+      enable = true;
+      cdi.dynamic.nvidia.enable = true;
+    };
   };
+
   boot.kernelParams = ["nvidia.NVreg_PreserveVideoMemoryAllocations=1"];
 
   environment.systemPackages = with pkgs-stable; [
     nvtop-nvidia
-    nvidia-docker
   ];
 
   services.xserver.videoDrivers = ["nvidia"];
 
   hardware = {
+    nvidia-container-toolkit = {
+      enable = true;
+      mount-nvidia-executables = true;
+    };
     nvidia = {
       modesetting.enable = true;
       powerManagement.enable = lib.mkDefault true;
