cb2d20ae9ecc4b314c6635e3e21d86892e7c0c14
atropos <sv7n@pm.me>
2024-12-21 10:06:11 +0000

    fixed a golang issue i had and removed dotnet 7 as its no longer supported.

--- lib/pkgs/csharp.nix
+++ lib/pkgs/csharp.nix
@@ -5,12 +5,10 @@
 
     # Dotnet SDK
     dotnet-sdk_8
-    dotnet-sdk_7
     dotnet-sdk # this is 6
 
     # Dotnet runtime
     dotnet-runtime_8
-    dotnet-runtime_7
     dotnet-runtime # this is 6
   ];
 }
--- lib/pkgs/go/default.nix
+++ lib/pkgs/go/default.nix
@@ -12,5 +12,18 @@
     delve
   ];
 
+  # INFO: I encountered an issue when trying to debug a test in go. This is the error message:
+  # Build Error: go test -c -o /home/atropos/projects/libs/gocore/__debug_bin3999853172 -gcflags all=-N -l ./pubsub
+  # # runtime/cgo
+  # In file included from /nix/store/wlavaybjbzgllhq11lib6qgr7rm8imgp-glibc-2.39-52-dev/include/bits/libc-header-start.h:33,
+  #                  from /nix/store/wlavaybjbzgllhq11lib6qgr7rm8imgp-glibc-2.39-52-dev/include/stdlib.h:26,
+  #                  from _cgo_export.c:3:
+  # /nix/store/wlavaybjbzgllhq11lib6qgr7rm8imgp-glibc-2.39-52-dev/include/features.h:414:4: error: #warning _FORTIFY_SOURCE requires compiling with optimization (-O) [-Werror=cpp]
+  #   414 | #  warning _FORTIFY_SOURCE requires compiling with optimization (-O)
+  #       |    ^~~~~~~
+  # cc1: all warnings being treated as errors (exit status 1)
+  # Setting the CGO_CFLAGS to -O2 fixes the issue. Could also set "-Werror" to not turn warnings into errors but thats less ideal.
+  environment.sessionVariables.CGO_CFLAGS = "-O2";
+
   home-manager.users.atropos.home.file.".golangci.yml".source = ./golangci.yml;
 }
