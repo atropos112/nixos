e75928d61e06018997ec377d9a50cbd2bc537a9e
atropos <sv7n@pm.me>
2025-02-23 09:26:30 +0000

    Fixed url for maligpu

--- lib/common/opi5/mali-firmware.nix
+++ lib/common/opi5/mali-firmware.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation {
   version = "unstable-2022-11-21";
 
   src = fetchurl {
-    url = "https://github.com/JeffyCN/rockchip_mirrors/raw/309268f7a34ca0bba0ab94a0b09feb0191c77fb8/firmware/g610/mali_csffw.bin";
+    url = "https://github.com/JeffyCN/mirrors/raw/309268f7a34ca0bba0ab94a0b09feb0191c77fb8/firmware/g610/mali_csffw.bin";
     hash = "sha256-rZFRGnf6EQr0aiopytniJVJNnPqf41vc6WYG01TmB94=";
   };
 
