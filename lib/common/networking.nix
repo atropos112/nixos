f8acb21c171b4c407de0925ba5ad6ce22aafcb25
atropos <sv7n@pm.me>
2025-03-28 12:18:55 +0000

    Not nftables to blame hmm

--- lib/common/networking.nix
+++ lib/common/networking.nix
@@ -2,7 +2,7 @@ _: {
   # Networking basics (hostname excluded)
   networking = {
     usePredictableInterfaceNames = false;
-    nftables.enable = false; # Has to be disabled as otherwise k3s doesn't start up.
+    nftables.enable = true;
     firewall.enable = false;
     enableIPv6 = false;
   };
