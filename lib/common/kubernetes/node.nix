60d03362cd45b3ede1320094091750398189bbe5
atropos <sv7n@pm.me>
2025-05-18 21:34:39 +0100

    kubeconfig on k8s nodes

--- lib/common/kubernetes/node.nix
+++ lib/common/kubernetes/node.nix
@@ -21,6 +21,15 @@ in {
     serverAddr = "https://11.0.0.11:6443";
   };
 
+  # Needed for grafana alloy but also convininient.
+  # Grafana alloy infact needs this to be called "kubeconfig" so it exists
+  # in /run/secrets/kubeconfig as well
+  sops.secrets."kubeconfig" = {
+    owner = "atropos";
+    path = "/home/atropos/.kube/config";
+    mode = "0444"; # Read only
+  };
+
   atro.fastfetch.extraModules = [
     {
       "type" = "command";
