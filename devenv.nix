46bb7218bffcab377fa91928cad7c01572b26508
atropos <sv7n@pm.me>
2025-06-28 21:13:27 +0100

    Added pipe operators to config and not devenv

--- devenv.nix
+++ devenv.nix
@@ -45,14 +45,14 @@ in {
 
     apply-local = {
       exec = writeShellScript "apply-local" ''
-        build && sudo nixos-rebuild switch --fast --flake .#$(hostname) || exit 1
+        build && sudo nixos-rebuild switch --no-reexec --flake .#$(hostname) || exit 1
       '';
       description = "Rebuild the system";
     };
 
     build = {
       exec = writeShellScript "build" ''
-        sudo ${pkgs.nix-output-monitor}/bin/nom build .#nixosConfigurations.$(hostname).config.system.build.toplevel --fallback -L --extra-experimental-features pipe-operators
+        sudo ${pkgs.nix-output-monitor}/bin/nom build .#nixosConfigurations.$(hostname).config.system.build.toplevel --fallback -L
       '';
       description = "Build the system";
     };
--- lib/common/nix.nix
+++ lib/common/nix.nix
@@ -38,6 +38,7 @@ in {
       experimental-features = [
         "nix-command"
         "flakes"
+        "pipe-operators"
       ];
     };
     gc = {
