732be0464d7625cf9d665f5f3884e91e3eddfbb5
atropos <sv7n@pm.me>
2025-08-02 18:27:11 +0100

    Watchdog updating names to account for recent systemd changes.

--- profiles/common/server.nix
+++ profiles/common/server.nix
@@ -22,25 +22,23 @@ in {
       flake = "github:atropos112/nixos#${shortHostName}";
     };
   };
+
+  # For more detail about watchdog, see:
+  #   https://0pointer.de/blog/projects/watchdog.html
+  # Enabling watchdog below.
   systemd = {
     # Given that our systems are headless, emergency mode is useless.
     # We prefer the system to attempt to continue booting so
     # that we can hopefully still access it remotely.
     enableEmergencyMode = false;
 
-    # For more detail, see:
-    #   https://0pointer.de/blog/projects/watchdog.html
-    watchdog = {
+    settings.Manager = {
       # systemd will send a signal to the hardware watchdog at half
       # the interval defined here, so every 10s.
       # If the hardware watchdog does not get a signal for 20s,
       # it will forcefully reboot the system.
-      runtimeTime = "20s";
-      # Forcefully reboot if the final stage of the reboot
-      # hangs without progress for more than 30s.
-      # For more info, see:
-      #   https://utcc.utoronto.ca/~cks/space/blog/linux/SystemdShutdownWatchdog
-      rebootTime = "30s";
+      RuntimeWatchdogSec = "20s"; # How often to ping the hardware watchdog
+      RebootWatchdogSec = "30s"; # How long to wait before rebooting if no progress is made
     };
 
     sleep.extraConfig = ''
