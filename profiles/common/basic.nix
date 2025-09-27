4ba5d62acd0b5e20fc9c6b41ff1e6098a0bf8ba2
atropos <sv7n@pm.me>
2025-09-27 08:50:14 +0100

    Revert "Reverting flake.lock"
    This reverts commit 758a91a40fc316c45e8e08d3ab08e9a34708bf8a.

--- profiles/common/basic.nix
+++ profiles/common/basic.nix
@@ -92,7 +92,7 @@ in {
     enableRedistributableFirmware = true;
   };
   boot = {
-    kernelPackages = lib.mkDefault pkgs.linuxPackages_6_15;
+    kernelPackages = lib.mkDefault pkgs.linuxPackages_6_16;
     binfmt.emulatedSystems =
       if (pkgs.system == "x86_64-linux")
       then ["aarch64-linux"]
