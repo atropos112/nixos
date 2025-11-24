bc910ad12466a82ce3009b408d29abbec9299bd9
atropos <sv7n@pm.me>
2025-11-24 21:48:43 +0000

    decided to just stick with mainline kernel and not worry each time 'is it the zen kernel causing this' even though zen kernel is always innocent.

--- hosts/giant/default.nix
+++ hosts/giant/default.nix
@@ -12,7 +12,7 @@
     ../../profiles/networking/dns/london.nix
   ];
 
-  boot.kernelPackages = pkgs.linuxPackages_zen;
+  boot.kernelPackages = pkgs.linuxPackages_6_17;
 
   home-manager.users.atropos.programs = {
     mods = {
