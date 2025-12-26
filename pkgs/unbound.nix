598abbc3e971cb3cec4244d9cd53dfeb16fa914a
atropos <sv7n@pm.me>
2025-12-26 12:29:15 +0000

    Added some comments in unbound

--- pkgs/unbound.nix
+++ pkgs/unbound.nix
@@ -1,21 +1,25 @@
 {pkgs-stable, ...}: {
+  # Unbound: recursive DNS resolver (no forwarders, queries root servers directly)
+  # Privacy note: QNAME minimization is on by default, which reduces info leaked to each
+  # nameserver in the chain. However, all queries are plaintext - ISP sees everything.
+  # For true DNS privacy you'd need DoH/DoT, but then you're trusting that resolver.
   services.unbound = {
     enable = true;
     package = pkgs-stable.unbound;
-    resolveLocalQueries = false;
-    checkconf = true;
-    enableRootTrustAnchor = false;
+    resolveLocalQueries = false; # Don't hijack /etc/resolv.conf
+    checkconf = true; # Validate config at build time
+    enableRootTrustAnchor = false; # Disable DNSSEC anchor fetch - prevents startup hang when network is flaky
     settings = {
       server = {
-        port = "5553";
+        port = "5553"; # Non-standard port, AdGuard Home connects here as upstream
         interface = ["0.0.0.0"];
         access-control = [
-          "0.0.0.0/0 allow" # Allow all IPv4
+          "0.0.0.0/0 allow" # Wide open but fine - only AdGuard Home talks to this
         ];
         do-ip4 = "yes";
         do-udp = "yes";
         do-tcp = "yes";
-        verbosity = "1"; # Increase for debugging
+        verbosity = "1";
       };
     };
   };
