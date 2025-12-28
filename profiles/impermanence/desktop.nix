180d461ba6d768f7ad782417ea872653304aa97c
atropos <sv7n@pm.me>
2025-12-28 12:28:23 +0000

    Add more dirs to impermanence and rearrange for easy reading.

--- profiles/impermanence/desktop.nix
+++ profiles/impermanence/desktop.nix
@@ -17,20 +17,22 @@ _: {
 
       # INFO: User dirs are relative to their home directory i.e. .ssh -> /home/atropos/.ssh
       dirs = [
-        ".wakatime" # Wakatime downloads some cache (some bins etc)
         "Sync" # Syncthing
         "projects" # Code projects
         "nixos" # NixOS config
+
         ".mozilla" # Firefox config
         ".ollama" # Ollama cache
         ".claude" # Claude config
+        ".wakatime" # Wakatime downloads some cache (some bins etc)
+
+        ".cache/ZapZap" # Whatsapp for linux cache.
+        ".cache/prek" # Cache for prek (pre-commit) hooks
+        ".cache/nvim" # Neovim cache
 
         ".config/nvim" # Neovim config
         ".config/Element" # Element desktop config.
         ".config/Signal" # Signal desktop config.
-        ".local/share/ZapZap" # Whatsapp for linux cache.
-        ".cache/ZapZap" # Whatsapp for linux cache.
-        ".cache/prek" # Cache for prek (pre-commit) hooks
         ".config/github-copilot"
         ".config/sops/age" # Allowing atropos user read and edit the age keys
         ".config/wakatime" # waka time local bin's otherwise it wipes the password. There is a WAKATIME_HOME environment variable pointing to this dir.
@@ -38,11 +40,11 @@ _: {
 
         ".local/share/nvim" # Neovim plugins and basic cache (Treesitter, etc.)
         ".local/share/Anki2" # Anki cache
-
-        ".cache/nvim" # Neovim cache
+        ".local/share/ZapZap" # Whatsapp for linux cache.
       ];
       files = [
         ".claude.json"
+        ".claude.json.backup"
       ];
     };
   };
